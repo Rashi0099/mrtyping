52941ed69e6eba57ee070fa3780d77d2216a3534
Rashi0099 <abdulrasheed5140@gmail.com>
2026-03-16 11:04:26 +0530

    new

--- game/admin.py
+++ game/admin.py
@@ -1,8 +1,2 @@
 from django.contrib import admin
-from .views import RegisterView,ProfileView,TypingResult,LevelCompletion,LeaderboardView
-# Register your models here.
-admin.site.register(RegisterView)
-admin.site.register(ProfileView)
-admin.site.register(TypingResult)
-admin.site.register(LevelCompletion)
-admin.site.register(LeaderboardView)
+
